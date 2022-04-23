243569287f3fbef4cbc450a466139475ee8b2f87
Iuri Mendes <fcoiuri@gmail.com>
2022-04-23 04:52:10 -0300

    Updates

---
+++ precache-manifest.5c6cfe6d8d9a8be1da914437af11b40e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4564a22186ca57dae1192329e69fb9",
+    "url": "/ReactForm/index.html"
+  },
+  {
+    "revision": "48ca60d1f5e5d6626e06",
+    "url": "/ReactForm/static/css/2.d047d057.chunk.css"
+  },
+  {
+    "revision": "e347b1ae332bf3c0956e",
+    "url": "/ReactForm/static/css/main.8ab6f918.chunk.css"
+  },
+  {
+    "revision": "48ca60d1f5e5d6626e06",
+    "url": "/ReactForm/static/js/2.58ad6b03.chunk.js"
+  },
+  {
+    "revision": "d14a90c0d8b4e186f0c9cd0c822c98e5",
+    "url": "/ReactForm/static/js/2.58ad6b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e347b1ae332bf3c0956e",
+    "url": "/ReactForm/static/js/main.a05a2a13.chunk.js"
+  },
+  {
+    "revision": "c773c15521ac2bd965d3",
+    "url": "/ReactForm/static/js/runtime-main.c9dde725.js"
+  },
+  {
+    "revision": "73f26bf98a715ecab4d2287ff3a02ad0",
+    "url": "/ReactForm/static/media/roboto-all-400-normal.73f26bf9.woff"
+  },
+  {
+    "revision": "ba2c6cb0af81f8da49a960db84f26b7c",
+    "url": "/ReactForm/static/media/roboto-cyrillic-400-normal.ba2c6cb0.woff2"
+  },
+  {
+    "revision": "2e0b8660b87034ecf56aa9f488fbc08e",
+    "url": "/ReactForm/static/media/roboto-cyrillic-ext-400-normal.2e0b8660.woff2"
+  },
+  {
+    "revision": "22786f243202d7912399ffd10c76fe78",
+    "url": "/ReactForm/static/media/roboto-greek-400-normal.22786f24.woff2"
+  },
+  {
+    "revision": "5cff07beab63ec777fc73ac0483811b0",
+    "url": "/ReactForm/static/media/roboto-greek-ext-400-normal.5cff07be.woff2"
+  },
+  {
+    "revision": "aa23b7b4bcf2b8f0e876106bb3de69c6",
+    "url": "/ReactForm/static/media/roboto-latin-400-normal.aa23b7b4.woff2"
+  },
+  {
+    "revision": "718dded3393324e992b225ac61329e0c",
+    "url": "/ReactForm/static/media/roboto-latin-ext-400-normal.718dded3.woff2"
+  },
+  {
+    "revision": "756af8e5560d200e53b52b1ff70f2ad0",
+    "url": "/ReactForm/static/media/roboto-vietnamese-400-normal.756af8e5.woff2"
+  }
+]);
--- src/contexts/RegisterValidation.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const RegisterValidation = React.createContext();
-
-export default RegisterValidation;
